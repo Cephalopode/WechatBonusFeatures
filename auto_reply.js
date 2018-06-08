6e7996b5d3587c3615d26a63c0bd40821f77d208
tangmu <tangmu@lancai.cn>
2018-06-08 15:50:03 +0800

    track messages content instead of tracking red dot

--- auto_reply.js
+++ auto_reply.js
@@ -1,5 +1,6 @@
 //setInterval(function(){	if(new Date().toLocaleString().indexOf('6/3/2013 8:00:00 PM')===0) { $('.chatSend')[0].click() }},1000);
 
+//simulate keyboard key press and send message
 sendMessage = (message) => {
 	$('.edit_area').html(message);
 	$(".edit_area").trigger($.Event("keydown", { keyCode: 13,ctrlKey: true}));
@@ -8,13 +9,33 @@ sendMessage = (message) => {
 	}
 
 //answer if new message
-autoAnswer = () => {
-if(!!$('#J_NavChatScrollBody > div > div:nth-child(2) .avatar .icon').length) {
-	$('#J_NavChatScrollBody > div > div:nth-child(2) > div').trigger('click')
-	nickname = $('#J_NavChatScrollBody > div > div:nth-child(2) > div > .info > .nickname > span').text()
-	answer = "嗨" + nickname + "，本回复是自动生成的~"
-	sendMessage(answer)
-	$('#J_NavChatScrollBody > div > div:nth-child(3) > div').trigger('click')
+autoAnswer = (params) => {
+	var {nickname,content,answer} = params
+	for(let i=0;i<10;i++) {
+		new_digest[i] = $(`#J_NavChatScrollBody > div > div:nth-child(${i}) > div > .info > .ng-scope > .ng-binding`).text()
+		if(old_digest.length > 0 && new_digest[i] != old_digest[i]) {
+			nickn = $(`#J_NavChatScrollBody > div > div:nth-child(${i}) > div > .info > .nickname > span`).text()
+			message = new_digest[i]
+			if(nickn == nickname && message.search(content)>-1) {
+				$(`#J_NavChatScrollBody > div > div:nth-child(${i}) > div`).trigger('click')
+				console.log('sent')
+				sendMessage(answer)
+				clearInterval(sh)
+				break
+			}
+		}
+		old_digest[i] = new_digest[i]
 	}
 }
-sh = setInterval(autoAnswer,100)
+
+params = {
+	nickname: "晚上吃辣",
+	content: "点饭",
+	answer: "+1"
+}
+
+let old_digest = []
+let new_digest = []
+
+sh = setInterval(()=>autoAnswer(params),100)
+
